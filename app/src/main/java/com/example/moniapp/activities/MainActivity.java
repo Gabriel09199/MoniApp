93624e6c96d18a79efb3cdef597f45a7ff1fec88
sercel98 <sergiocelemin1998@gmail.com>
2019-02-24 17:47:14 -0500

    Prueba con Main
    Se ajustó el Main acrtivity para cel J7 prime

--- app/src/main/java/com/example/moniapp/activities/MainActivity.java
+++ app/src/main/java/com/example/moniapp/activities/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.moniapp.activities;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.moniapp.R;
 import com.example.moniapp.servicios.ServicioMoniApp;
@@ -12,12 +15,38 @@ public class MainActivity extends AppCompatActivity
 {
     private ServicioMoniApp servicio;
 
+    private Typeface fontBold;
+
+
+    private TextView txtNameApp;
+
+    private Button btnTutor;
+
+    private Button btnUsuario;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         servicio = new ServicioMoniApp();
+        fontBold = Typeface.createFromAsset(getAssets(),"Montserrat-Bold.ttf");
+
+
+
+        txtNameApp = (TextView) findViewById(R.id.txtNameApp);
+        txtNameApp.setTypeface(fontBold);
+
+        btnTutor = (Button) findViewById(R.id.btnTutor);
+        btnTutor.setTypeface(fontBold);
+
+        btnUsuario = (Button) findViewById(R.id.btnUsuario);
+        btnUsuario.setTypeface(fontBold);
+
+
+
+
     }
 
     public void btnIniciarVistaUsuario(View view)
